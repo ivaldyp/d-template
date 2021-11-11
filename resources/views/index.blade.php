3c25d7c09114817105c6c610d3db7d01f1c58765
Ivaldy Putra <vivaaldy@gmail.com>
2021-11-11 14:36:31 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in (v2)</title>
+
+  <!-- Bootstrap 5.1.3 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('lte/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css') }}">
+
+  <style>
+    #thisbody {
+      /* background: #24C6DC; 
+      background: -webkit-linear-gradient(to right, #514A9D, #24C6DC); 
+      background: linear-gradient(to right, #514A9D, #24C6DC); */
+
+      background: #5C258D; 
+      background: -webkit-linear-gradient(to right, #4389A2, #5C258D); 
+      background: linear-gradient(to right, #4389A2, #5C258D);
+    }
+  </style>
+</head>
+<body class="hold-transition login-page" id="thisbody">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <!-- <a href="../../index2.html" class="h1"><b>BMD</b> DKI Jakarta</a> -->
+        <img src="{{ url('public/img/bpad-logo-2.png') }}" width="100%">
+    </div>
+    <div class="card-body login-card-body">
+      <form action="../../index3.html" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-5">
+            <a href="">
+              <button type="button" style="text-align: left; border: 0px;" class="btn btn-default btn-block"><i class="fas fa-arrow-left"></i> Kembali</button>
+            </a>
+          </div>
+          <div class="col-4 offset-md-3">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- Bootstrap 5.1.3 -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- jQuery -->
+<script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('lte/dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
